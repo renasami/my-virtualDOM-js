03641f2eaf001ad81d7a418a30c4d7b24dae4782
renasami <drrr0502@gmail.com>
2021-06-27 21:45:59 +0900

    add vDomNext

--- vDomEx.js
+++ vDomEx.js
@@ -1,10 +1,37 @@
 var vDomEx = {
-    "tagName": "p",
-    "attrs": {
-      class: "text"
+  nodeName: "div",
+  attributes: { id: "app" },
+  children:[
+    {
+      nodeName: 'p',
+      attributes: { class: "text" },
+      children: ["Hello Virtual DOM!!"]
+    }
+  ]
+}
+
+
+const app = document.getElementById('app');
+
+const p = document.createElement('h2');
+p.innerText = 'ラーメンが食べたい';
+
+app.appendChild(p);
+
+
+var vDomNext = {
+  nodeName: "div",
+  attributes: { id: "app" },
+  children:[
+    {
+      nodeName: 'p',
+      attributes: { class: "text" },
+      children: ["Hello Virtual DOM!!"]
     },
-    "children": [
-      "Hello virtual DOM!"
-    ]
-  }
-  
+    {
+      nodeName: 'h2',
+      attributes: {},
+      children: ["ラーメン食べたい"]
+    }
+  ]
+}
