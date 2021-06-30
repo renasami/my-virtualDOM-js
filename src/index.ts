60977b8337ec4c6d99e5f01144b9f275df94850c
renasami <drrr0502@gmail.com>
2021-06-30 21:38:34 +0900

    exCode Change

--- src/index.ts
+++ src/index.ts
@@ -6,54 +6,43 @@ type State = typeof state;
 type Actions = typeof actions;
 
 const state = {
-  count: 0
-}
-// const state = {
-//   img: ["itagaki.png","pien.png"],
-//   n: 0
-// };
+  img: ["itagaki.png","pien.png"],
+  imgs: ["itagaki.png"],
+  n: 0
+};
 
-
-const actions: ActionTree<State> = {
+const actions: ActionTree <State>= {
   increment: (state: State) => {
-    state.count++;
-    console.log(state.count)
+    if (state.n == 0){
+      state.n = 1
+      state.imgs.push(state.img[state.n])
+      return 
+    }
+    state.n = 0
+    state.imgs.push(state.img[state.n])
+    console.log(state.imgs)
+    return
   }
 };
 
-// const actions: ActionTree <State>= {
-//   increment: (state: State) => {
-//     if (state.n == 0){
-//       state.n = 1
-//       state.img[state.n]
-//       return 
-//     }
-//     state.n = 0
-//     state.img[state.n]
-//     return
-//   }
-// };
-
-// const view: View<State, Actions> = (state, actions) => {
-//   return h(
-//     "div",
-//     null,
-//     h("img", {src: "./images/" + state.img[state.n] }, ""),
-//     h(
-//       "button",
-//       { type: "button", onclick: () => actions.increment(state) },
-//       "change"
-//     )
-//   );
-// };
-
-const view: View<State, Actions> = (state, actions) => h("div", { id: "app" },
-  h("p", { id: "counter" }, state.count ),
+const view: View<State, Actions> = (state, actions) => h("div", { id: "countup" },
   h("button", {
     type: "button",
     id: "increment",
     onclick: () => { actions.increment(state); }},
-    "+1" 
+    "change" 
+  ),
+  h("ul",
+    {class: "ul", style:" list-style: none;"},
+    ...state.imgs.map(a => {
+      return h(
+      "li", 
+      {class:"li"}, 
+      h("img", 
+      { src: "images/" + a, style: "height: 50px; width: 50px;" }, 
+      state.img[state.n]), 
+      "")
+    })
   )
 );
 
